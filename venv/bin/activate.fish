69dfb2da94f2ca54da6a7b27b446d53287214a81
LJS0000 <jisu2281l@naver.com>
2023-07-24 16:00:28 +0900

    🔐 SECURE: dotenv 설정

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jisu/Documents/develope/myblog/venv"
+set -gx VIRTUAL_ENV "/Users/jisu/Documents/develope/blog/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
