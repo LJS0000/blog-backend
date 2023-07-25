c3ed1b3f2ea05186320fedeb9dd523961b3086d7
LJS0000 <jisu2281l@naver.com>
2023-07-25 11:26:56 +0900

    🎨 ADD: DEFAULT_RENDERER_CLASSES 설정 추가

--- app/settings.py
+++ app/settings.py
@@ -98,6 +98,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "app.wsgi.application"
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
