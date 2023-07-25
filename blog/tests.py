3812d755ad17db5eb986befe2ef2bf083ae74ae1
LJS0000 <jisu2281l@naver.com>
2023-07-25 14:49:32 +0900

    🎨 ADD: 글 수정 뷰 및 테스트코드 작성

--- blog/tests.py
+++ blog/tests.py
@@ -6,21 +6,25 @@ from .models import Post
 
 
 class PostAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='testpassword')
+
     def test_list_posts(self):
         # User 인스턴스 생성
-        test_user1 = User.objects.create_user(username='testuser1', password='12345')
-        test_user2 = User.objects.create_user(username='testuser2', password='12345')
 
         # 테스트용 데이터 생성
         Post.objects.create(
-            title='Test title 1', content='Test content 1', author=test_user1
-        )
-        Post.objects.create(
-            title='Test title 2', content='Test content 2', author=test_user2
+            title='Test title 1', content='Test content 1', author=self.user
         )
 
         url = reverse('blog:posts')
         response = self.client.get(url, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_post(self):
+        url = reverse('blog:post-write')
+        data = {'title': 'Title 1', 'content': 'Content 1', 'author': self.user.id}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
